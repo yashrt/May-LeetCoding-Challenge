669cbf580e9ececadad5181ba1676e3a63891285
Yash Rathod <rathody468@gmail.com>
2020-05-01 23:31:33 +0530

    Day-01 'First Bad Version' solution

---
+++ Day-01.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid))
+            {
+                if(!isBadVersion(mid-1))    return mid;
+                high = mid-1;
+            }
+            else
+                low = mid+1;
+        }
+        return -1;
+    }
+};
