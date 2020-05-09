c353421b14a50a9073ad436f18f6ee42978b8585
Yash Rathod <rathody468@gmail.com>
2020-05-09 23:57:18 +0530

    Day-09 'Valid Perfect Square' solution added

---
+++ Day-09.cpp
@@ -0,0 +1,20 @@
+// Using the Binary Search to check Perfect square
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low = 1, high = num;
+        
+        while(low <= high)
+        {
+            long mid = low + (high - low)/2;
+            if(mid*mid == num)
+                return true;
+            else if(mid*mid < num)
+                low = mid +1;
+            else
+                high = mid -1;
+        }
+        return false;
+    }
+};
